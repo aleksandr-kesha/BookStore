dea140782223dcfd81e34827e0cf9a78af2f5b97
Aleksandr Smalyokha <aleksandr.smalyokha@mitratech.com>
2017-05-04 14:52:36 +0700

    Initial commit

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BookContext _db = new BookContext();
+
+        public ActionResult Index()
+        {
+            var books = _db.Books.ToList();
+
+            ViewBag.Books = books;
+
+            return View();
+        }    
+    }
+}
